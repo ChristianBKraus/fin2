202b5cb9908f11bfd71147272b0719902d2d5b5c
ChristianBKraus <Christian.Kraus@sap.com>
2019-11-14 20:04:23 +0100

    Initial 2

---
+++ fin2-model/src/main/java/fin2/model/SalesDocument.java
@@ -0,0 +1,36 @@
+package fin2.model;
+
+import fin2.util.IKey;
+import lombok.*;
+
+import java.util.List;
+
+@Data @Builder
+public class SalesDocument implements IKey {
+    static long id_index = 1;
+
+    long salesDocumentId;
+    String documentDate;
+    String customerId;
+    List<SalesDocumentItem> items;
+
+    public SalesDocument(long salesDocumentId, String documentDate, String customerId,
+                         List<SalesDocumentItem> items) {
+        this.salesDocumentId = salesDocumentId;
+        setId();
+        this.documentDate = documentDate;
+        this.customerId = customerId;
+        this.items = items;
+    }
+
+    private void setId() {
+        if (salesDocumentId == 0L)
+            this.salesDocumentId = id_index;
+        id_index++;
+    }
+
+    public String getKey() {
+        return String.format("%d",salesDocumentId);
+    }
+
+}
